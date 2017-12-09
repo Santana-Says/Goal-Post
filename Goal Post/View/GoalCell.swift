bbe37fd5edd8992de1eba62fb2df68c733088769
Jeffrey Santana <santana@jefffreys-mini.attlocal.net>
2017-12-09 15:51:52 -0500

    added tableview to UI, created tableviewcell & it's cocoa touch class

---
+++ Goal Post/View/GoalCell.swift
@@ -0,0 +1,24 @@
+//
+//  GoalCell.swift
+//  Goal Post
+//
+//  Created by Jeffrey Santana on 12/9/17.
+//  Copyright © 2017 Jefffrey Santana. All rights reserved.
+//
+
+import UIKit
+
+class GoalCell: UITableViewCell {
+
+    //Outlets
+    @IBOutlet weak var goalDescriptionLbl: UILabel!
+    @IBOutlet weak var goalTypeLbl: UILabel!
+    @IBOutlet weak var goalProgressLbl: UILabel!
+    
+    func configCell(goalDescription desc: String, goalType type: String, goalProgressAmount days: Int) {
+        goalDescriptionLbl.text = desc
+        goalTypeLbl.text = type
+        goalProgressLbl.text = "\(days)"
+    }
+    
+}
