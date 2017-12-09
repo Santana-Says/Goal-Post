bf87ebf0a3ff5faec71a79b9aeb100e7f8bc8659
Jeffrey Santana <santana@jefffreys-mini.attlocal.net>
2017-12-09 15:18:42 -0500

    inital commit with basic UI

---
+++ Goal Post/Controller/GoalsVC.swift
@@ -0,0 +1,27 @@
+//
+//  GoalsVC.swift
+//  Goal Post
+//
+//  Created by Jeffrey Santana on 12/9/17.
+//  Copyright © 2017 Jefffrey Santana. All rights reserved.
+//
+
+import UIKit
+
+class GoalsVC: UIViewController {
+
+    //Outlets
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    //Variables
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func addGoalBtnPressed(_ sender: Any) {
+    }
+    
+}
+
