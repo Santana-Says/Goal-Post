2db874d8d831352d95f13b4dd9fcdcca7417f6a3
Jeffrey Santana <santana@jefffreys-mini.attlocal.net>
2017-12-13 00:41:02 -0500

    added a view to appear over cell when complete and a tableview action to increment the goal counter

--- Goal Post/Controller/GoalsVC.swift
+++ Goal Post/Controller/GoalsVC.swift
@@ -28,8 +28,8 @@ class GoalsVC: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super .viewWillAppear(animated)
-        tableView.reloadData()
         fetchCoreDataObjects()
+        tableView.reloadData()
     }
 
     @IBAction func addGoalBtnPressed(_ sender: Any) {
@@ -73,12 +73,37 @@ extension GoalsVC: UITableViewDelegate, UITableViewDataSource {
             tableView.deleteRows(at: [indexPath], with: .automatic)
         }
         
+        let addAction = UITableViewRowAction(style: .normal, title: "ADD 1") { (rowAction, indexPath) in
+            self.setProgress(atIndexPath: indexPath)
+            tableView.reloadRows(at: [indexPath], with: .automatic)
+        }
+        
         deleteAction.backgroundColor = #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1)
-        return [deleteAction]
+        addAction.backgroundColor = #colorLiteral(red: 0.9771530032, green: 0.7062081099, blue: 0.1748393774, alpha: 1)
+        return [deleteAction, addAction]
     }
 }
 
 extension GoalsVC {
+    
+    func setProgress(atIndexPath indexPath: IndexPath) {
+        guard let managedContext = appDelegate?.persistentContainer.viewContext else { return }
+        let chosenGoal = goals[indexPath.row]
+        
+        if chosenGoal.goalProgressCounter < chosenGoal.goalCompletionValue {
+            chosenGoal.goalProgressCounter += 1
+        } else {
+            return
+        }
+        
+        do {
+            try managedContext.save()
+            print("Succefully updated progress")
+        } catch {
+            debugPrint("Could not set progress \(error.localizedDescription)")
+        }
+        
+    }
     
     func removeGoal(atIndexPath indexPath: IndexPath) {
         guard let managedContext = appDelegate?.persistentContainer.viewContext else { return }
--- Goal Post/View/GoalCell.swift
+++ Goal Post/View/GoalCell.swift
@@ -14,11 +14,18 @@ class GoalCell: UITableViewCell {
     @IBOutlet weak var goalDescriptionLbl: UILabel!
     @IBOutlet weak var goalTypeLbl: UILabel!
     @IBOutlet weak var goalProgressLbl: UILabel!
+    @IBOutlet weak var completionView: UIView!
     
     func configCell(goal: Goal) {
         goalDescriptionLbl.text = goal.goalDescription
         goalTypeLbl.text = goal.goalType
         goalProgressLbl.text = "\(goal.goalProgressCounter)"
+        
+        if goal.goalProgressCounter == goal.goalCompletionValue {
+            completionView.isHidden = false
+        } else {
+            completionView.isHidden = true
+        }
     }
     
 }
